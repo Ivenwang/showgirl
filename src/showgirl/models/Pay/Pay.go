bcc8d79d4e0a3924e697bea6be892d83bcaefa8b
Ivenwang <235886253@qq.com>
2017-12-18 09:20:41 +0800

    update pay

--- src/showgirl/models/Pay/Pay.go
+++ src/showgirl/models/Pay/Pay.go
@@ -1,19 +1,25 @@
 package Pay
 
 import (
+	"bytes"
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/xml"
+	"errors"
 	"showgirl/client"
 	"showgirl/models/utils"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/gogo/protobuf/proto"
 )
 
 const WXAppID = "wx67cb0c8656d9400c"
 const WXMchID = "xxx"
 const NotifyURL = "https://grassua.site/callpack/pay"
 const PaySignKey = "xxx"
+const WxPayURL = "https://api.mch.weixin.qq.com/pay/unifiedorder"
 
 //WXPayCreateOrderReq 创建支付订单
 type WXPayCreateOrderReq struct {
@@ -37,6 +43,22 @@ type WXPayCreateOrderReq struct {
 	TradeType      string `xml:"trade_type"`       //交易类型
 	LimitPay       string `xml:"limit_pay"`        //指定支付方式
 	OpenID         string `xml:"openid"`           //用户标识
+}
+
+//WXPayCreateOrderRsp 支付回包
+type WXPayCreateOrderRsp struct {
+	ReturnCode string `xml:"return_code"`  //返回码，通讯标识
+	ReturnMsg  string `xml:"return_msg"`   //通讯错误码
+	AppID      string `xml:"appid"`        //小程序id
+	MchID      string `xml:"mch_id"`       //商户号
+	DeviceInfo string `xml:"device_info"`  //设备号
+	NonceStr   string `xml:"nonce_str"`    //随机字符串
+	Sign       string `xml:"sign"`         //签名
+	ResultCode string `xml:"result_code"`  //业务结果
+	ErrCode    string `xml:"err_code"`     //错误代码
+	ErrCodeDes string `xml:"err_code_des"` //错误返回的信息描述
+	TradeType  string `xml:"trade_type"`   //交易类型
+	PrepayID   string `xml:"prepay_id"`    //预支付交易会话标识
 }
 
 //CreateTransaction 创建订单
@@ -69,7 +91,51 @@ func CreateTransaction(IPAddr string, FeeAmount int32, OpenID string, flowid int
 
 	stCreateOrder.Sign = GenSignByPay(stCreateOrder, flowid)
 
-	//发送支付请求
+	//序列化
+	reqBody, err := xml.Marshal(stCreateOrder)
+	if err != nil {
+		utils.Warn(flowid, "CreateTransaction xml.Marshal error, FeeAmount = %d, error = %s",
+			FeeAmount, err.Error())
+		return stCreateTransaction, err
+	}
+
+	//构造请求URL
+	rspData, err := utils.SendAndRecv("POST", WxPayURL, bytes.NewBuffer(reqBody), nil, flowid)
+	if err != nil {
+		utils.Warn(flowid, "CreateTransaction SendAndRecv error, WxPayURL = %s, reqBody = %s, err = %s",
+			WxPayURL, reqBody, err.Error())
+		return stCreateTransaction, err
+	}
+
+	utils.Debug(flowid, "CreateTransaction debug, WxPayURL = %s, reqBody = %s, rspData = %s",
+		WxPayURL, reqBody, rspData)
+
+	//解包
+	stCreateOrderRsp := &WXPayCreateOrderRsp{}
+	err = xml.Unmarshal(rspData, stCreateOrderRsp)
+	if err != nil {
+		utils.Warn(flowid, "CreateTransaction xml.Unmarshal error, WxPayURL = %s, reqBody = %s, rspData = %s, err = %s",
+			WxPayURL, reqBody, rspData, err.Error())
+		return stCreateTransaction, err
+	}
+
+	//通信错误
+	if stCreateOrderRsp.ReturnCode != "SUCCESS" {
+		utils.Warn(flowid, "CreateTransaction check ReturnCode error, WxPayURL = %s, reqBody = %s, rspData = %s",
+			WxPayURL, reqBody, rspData)
+		return stCreateTransaction, errors.New("check rsp error")
+	}
+
+	stCreateTransaction.AppId = proto.String(stCreateOrderRsp.AppID)
+	stCreateTransaction.MchID = proto.String(stCreateOrderRsp.MchID)
+	stCreateTransaction.DeviceInfo = proto.String(stCreateOrderRsp.DeviceInfo)
+	stCreateTransaction.NonceStr = proto.String(stCreateOrderRsp.NonceStr)
+	stCreateTransaction.Sign = proto.String(stCreateOrderRsp.Sign)
+	stCreateTransaction.ResultCode = proto.String(stCreateOrderRsp.ResultCode)
+	stCreateTransaction.ErrCode = proto.String(stCreateOrderRsp.ErrCode)
+	stCreateTransaction.ErrCodeDes = proto.String(stCreateOrderRsp.ErrCodeDes)
+	stCreateTransaction.TradeType = proto.String(stCreateOrderRsp.TradeType)
+	stCreateTransaction.PrepayID = proto.String(stCreateOrderRsp.PrepayID)
 
 	return stCreateTransaction, nil
 }
